6c60c0703c339d157ca55236a47b51fe096c9154
Angus Tasker <atasker2@gmail.com>
2015-08-12 15:39:06 -0400

    nagios initial commit

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.2.1'
+gem 'sass-rails', '~> 5.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.1.0'
+gem 'mechanize'
+gem 'nokogiri'
+gem 'turbolinks'
+gem 'gon'
+# See https://github.com/rails/execjs#readme for more supported runtimes
+# gem 'therubyracer', platforms: :ruby
+gem 'jquery-rails'
+gem 'jbuilder', '~> 2.0'
+gem 'sdoc', '~> 0.4.0', group: :doc
+group :development, :test do
+  # Access an IRB console on exception pages or by using <%= console %> in views
+  gem 'web-console', '~> 2.0'
+  # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
+  gem 'spring'
+end
+
---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,27 @@
+class WelcomeController < ApplicationController
+
+  def index
+    agent = Mechanize.new
+    agent.user_agent_alias = "Mac Safari"
+    agent.add_auth('http://nagios.playrific.com/nagios/cgi-bin/status.cgi?host=all', 'angus', 'thomas8185.')
+    agent.get('http://nagios.playrific.com/nagios/cgi-bin/status.cgi?host=all') do |page|
+      @title = page.title
+      @html = page.body
+    end
+
+    @doc = Nokogiri::HTML(@html)
+    @statusOK = @doc.css('td.statusOK').count
+    @statusCRITICAL = @doc.css('td.statusCRITICAL').count
+    @statusWARNING = @doc.css('td.statusWARNING').count
+
+    @messages = []
+    @messagesCRITICAL = @doc.css('td.statusBGCRITICAL a').each do |message|
+      @messages << message.inner_html
+    end
+
+    gon.statusOK = @statusOK
+    gon.statusWARNING = @statusWARNING
+    gon.statusCRITICAL = @statusCRITICAL
+  end
+
+end
